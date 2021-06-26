36a65b2cbddc06e7f68a9b002a42bdafc9b1512e
pavan teja <pavanteja6690@gmail.com>
2021-06-26 10:10:51 +0530

    done

--- index.js
+++ index.js
@@ -43,5 +43,5 @@ app.post("/admin/sendmail", function (req, res) {
   main(req.body.msg, req.body.to);
   //   console.log("called");
 });
-
-app.listen(5000);
+var PORT = process.env.PORT || 5000;
+app.listen(PORT);
